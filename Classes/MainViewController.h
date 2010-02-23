fdf3644f012e61800e02f6cc261ca017142c53cc
Matthew Moore <matt@thrivesmart.com>
2010-02-23 15:22:02 -0800

    audio recording works

--- Classes/MainViewController.h
+++ Classes/MainViewController.h
@@ -8,16 +8,33 @@
 
 #import "FlipsideViewController.h"
 #import "FBConnect/FBConnect.h"
+#import <AVFoundation/AVFoundation.h>
+#import <CoreAudio/CoreAudioTypes.h>
 
-@interface MainViewController : UIViewController <FlipsideViewControllerDelegate, FBDialogDelegate, FBSessionDelegate, FBRequestDelegate> {
+@interface MainViewController : UIViewController 
+	<FlipsideViewControllerDelegate, FBDialogDelegate, FBSessionDelegate, FBRequestDelegate, AVAudioRecorderDelegate> {
 	IBOutlet UILabel* _label;
 	IBOutlet FBLoginButton* _loginButton;
+	IBOutlet UIButton* _recordButton;
 	FBSession* _session;
+	AVAudioRecorder *recorder;
+	AVAudioPlayer *player;
+	NSString* _recordingFile;
+	BOOL _recording;
+	NSMutableDictionary* _recordingSettings;
 }
 
 @property(nonatomic,readonly) UILabel* label;
+@property(nonatomic,readonly) UIButton* recordButton;
+@property(nonatomic) BOOL recording;
 
 - (IBAction)showInfo;
+- (IBAction)recordButtonClicked;
 - (void)askPermission:(id)target;
+- (void)resetRecordButton;
+- (void)startRecording;
+- (void)stopRecording;
+- (void)handleRecordingStopped;
+- (void)playRecording;
 
 @end
--- Classes/MainViewController.m
+++ Classes/MainViewController.m
@@ -6,6 +6,8 @@
 //  Copyright ThriveSmart, LLC 2010. All rights reserved.
 //
 
+#define AUDIO_SAVE_DIR [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"]
+
 #import "MainViewController.h"
 #import "MainView.h"
 
@@ -15,9 +17,14 @@ static NSString* kApiSecret = @"c28af43643516490f1fb95ceb4aefb42";
 static NSString* kGetSessionProxy = nil; // @"<YOUR SESSION CALLBACK)>";
 
 
+
 @implementation MainViewController
 
+UInt32 _sounds[1];
+
+@synthesize recording = _recording;
 @synthesize label = _label;
+@synthesize recordButton = _recordButton;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
 	if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
@@ -33,7 +40,15 @@ static NSString* kGetSessionProxy = nil; // @"<YOUR SESSION CALLBACK)>";
 
 
 - (void)dealloc {
+	// Facebook dealloc
   [_session release];
+	
+	// AVAudioRecorder dealloc
+	[recorder release];
+	[_recordingFile release];
+	[_recordingSettings release];
+	[player	release];
+	
 	[super dealloc];
 }
 
@@ -43,9 +58,14 @@ static NSString* kGetSessionProxy = nil; // @"<YOUR SESSION CALLBACK)>";
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
+	[super viewDidLoad];
+	
+	// Facebook Initialize Code
 	[_session resume];
 	_loginButton.style = FBLoginButtonStyleWide;
-	[super viewDidLoad];
+	
+	// AVAudioRecorder Initialize Code
+	
 }
 
 - (void)viewDidUnload {
@@ -101,6 +121,16 @@ static NSString* kGetSessionProxy = nil; // @"<YOUR SESSION CALLBACK)>";
 }
 
 
+- (IBAction)recordButtonClicked {
+	if (_recording) {
+		[self stopRecording];
+	}
+	else {
+		[self startRecording];
+	}
+	
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // FBDialogDelegate
@@ -153,6 +183,146 @@ static NSString* kGetSessionProxy = nil; // @"<YOUR SESSION CALLBACK)>";
 }
 
 
+////////
+
+
+- (void) resetRecordButton {
+	[_recordButton setTitle:@"Record" forState:UIControlStateNormal];
+	_recording = NO;
+}
+
+- (void) startRecording {
+	[_recordButton setTitle:@"Stop" forState:UIControlStateNormal];
+	
+	AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+	NSError *err = nil;
+	[audioSession setCategory :AVAudioSessionCategoryPlayAndRecord error:&err];
+	if(err){
+		NSLog(@"audioSession: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+		return;
+	}
+	[audioSession setActive:YES error:&err];
+	err = nil;
+	if(err){
+		NSLog(@"audioSession: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+		return;
+	}
+	
+	if (!_recordingSettings) {
+		_recordingSettings = [[NSMutableDictionary alloc] init];
+		[_recordingSettings setValue :[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
+		[_recordingSettings setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey]; 
+		[_recordingSettings setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+		[_recordingSettings setValue :[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+		[_recordingSettings setValue :[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsBigEndianKey];
+		[_recordingSettings setValue :[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsFloatKey];
+	}
+	
+	// Create a new dated file
+	NSDate *now = [NSDate dateWithTimeIntervalSinceNow:0];
+	NSString *caldate = [now description];
+	_recordingFile = [[NSString stringWithFormat:@"%@/%@.caf", AUDIO_SAVE_DIR, caldate] retain];
+	
+	NSURL *url = [NSURL fileURLWithPath:_recordingFile];
+	err = nil;
+	recorder = [[ AVAudioRecorder alloc] initWithURL:url settings:_recordingSettings error:&err];
+	if (!recorder) {
+		NSLog(@"recorder: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+		UIAlertView *alert =
+		[[UIAlertView alloc] initWithTitle: @"Warning"
+															 message: [err localizedDescription]
+															delegate: nil
+										 cancelButtonTitle: @"OK"
+										 otherButtonTitles: nil];
+		[alert show];
+		[alert release];
+		[self resetRecordButton];
+		return;
+	}
+	
+	//prepare to record
+	[recorder setDelegate:self];
+	[recorder prepareToRecord];
+	recorder.meteringEnabled = YES;
+	
+	BOOL audioHWAvailable = audioSession.inputIsAvailable;
+	if (! audioHWAvailable) {
+		UIAlertView *cantRecordAlert =
+		[[UIAlertView alloc] initWithTitle: @"Warning"
+															 message: @"Audio input hardware not available"
+															delegate: nil
+										 cancelButtonTitle:@"OK"
+										 otherButtonTitles:nil];
+		[cantRecordAlert show];
+		[cantRecordAlert release]; 
+		return;
+	}
+	
+	// start recording
+	[recorder recordForDuration:(NSTimeInterval) 120];
+	_recording = YES;
+}
+
+- (void) stopRecording {
+	[recorder stop];
+	[self handleRecordingStopped];
+}
+
+- (void) playRecording {
+	NSURL *url = [NSURL fileURLWithPath: _recordingFile];
+	NSError *err;
+	if (player) {
+		[player release];
+		player = nil;
+	}
+	player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&err];
+	player.numberOfLoops = 0;
+	
+	if (player == nil) {
+		UIAlertView *cantPlayAlert =
+		[[UIAlertView alloc] initWithTitle: @"Can't Play Recording"
+															 message: [err localizedDescription]
+															delegate: nil
+										 cancelButtonTitle:@"OK"
+										 otherButtonTitles:nil];
+		[cantPlayAlert show];
+		[cantPlayAlert release];
+	}
+	else {
+		[player play];
+	}
+}
+
+- (void) handleRecordingStopped {
+	NSURL *url = [NSURL fileURLWithPath: _recordingFile];
+	NSError *err = nil;
+	NSData *audioData = [NSData dataWithContentsOfFile:[url path] options: 0 error:&err];
+	if(!audioData) {
+		NSLog(@"audio data: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+	}
+	
+	//[editedObject setValue:[NSData dataWithContentsOfURL:url] forKey:editedFieldKey];       
+	//[recorder deleteRecording];
+	
+	
+	//NSFileManager *fm = [NSFileManager defaultManager];
+	//err = nil;
+	//[fm removeItemAtPath:[url path] error:&err];
+	//if(err) {
+	//	NSLog(@"File Manager: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+	//}
+
+	[self playRecording];
+	
+	[self resetRecordButton];
+}
+
+- (void)audioRecorderDidFinishRecording:(AVAudioRecorder *) aRecorder successfully:(BOOL)flag {
+	[self handleRecordingStopped];
+	
+	NSLog (@"audioRecorderDidFinishRecording:successfully:");
+	// your actions here
+}
 
 
 @end
